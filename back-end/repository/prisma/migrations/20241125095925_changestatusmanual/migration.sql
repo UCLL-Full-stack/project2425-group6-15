035da3f894176d9635410ee87c13d8d168fab716
nathanP-ucll <145967892+nathanP-ucll@users.noreply.github.com>
2024-12-01 14:58:43 +0100

    migraition fix

--- back-end/repository/prisma/migrations/20241125095925_changestatusmanual/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `accepted` on the `Participant` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Participant" DROP COLUMN "accepted",
-ALTER COLUMN "status" DROP DEFAULT;
--- front-end/components/dashboard/sidebar.tsx
+++ front-end/components/dashboard/sidebar.tsx
@@ -13,12 +13,12 @@ const ActivityTopOverview: React.FC<ActivityTopOverviewProps> = ({ posts }) => {
   return (
     <>
       <div className="h-full bg-white shadow-lg rounded-lg p-6">
-          <h2 className="text-2xl font-bold text-slate-700 mb-4">Sidebar</h2>
+          <h2 className="text-2xl font-bold text-slate-700 mb-4">Posts</h2>
           <div className="w-full h-full flex flex-col">
             { posts.length === 0 && <p className="text-slate-500">No posts available</p> }
             {
               posts.map((post) => (
-                <div key={post.id} className="w-full h-20 bg-blue-200 rounded-lg mb-2 cursor-pointer" onClick={() => router.push(`/dashboard/posts/${post.id}`)}>
+                <div key={post.id} className="w-full h-20 border-t-2 border-slate-500 p-2 cursor-pointer" onClick={() => router.push(`/dashboard/posts/${post.id}`)}>
                   <h3 className="text-lg font-semibold text-slate-700">{post.title}</h3>
                   <p className="text-sm text-slate-500">{post.description}</p>
                 </div>
