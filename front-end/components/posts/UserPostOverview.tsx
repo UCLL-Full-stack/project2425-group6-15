e97ec43232646b4d836c82cb828c4111742c86a6
HannesBosmans <hannes.bosmans@student.ucll.be>
2024-12-16 14:56:13 +0100

    joined posts in post page + translations

--- front-end/components/posts/UserPostOverview.tsx
+++ front-end/components/posts/UserPostOverview.tsx
@@ -1,11 +1,13 @@
 import { PostPrevieuw } from "@/types";
+import { GetStaticProps } from "next";
 import React, { useState, useEffect } from "react";
 import userService from "@/services/userService";
 import { useTranslation } from "react-i18next"; // Add this import
 
 const UserPostOverview: React.FC = () => {
   const [posts, setPosts] = useState<PostPrevieuw[]>([]);
-  const { t } = useTranslation(); // Add this line
+  const [joinedPosts, setJoinedPosts] = useState<PostPrevieuw[]>([]);
+  const { t, i18n } = useTranslation(); // Add this line
   useEffect(() => {
     loadUserPosts();
   }, []);
@@ -17,12 +19,13 @@ const UserPostOverview: React.FC = () => {
     }
     const userData = await response.json();
     setPosts(userData.posts);
+    setJoinedPosts(userData.joinedPosts);
   };
 
   return (
     <>
       <div className="container grid grid-cols-1 gap-4 h-screen max-h-screen min-w-full text-gray-800 box-border pt-24 pb-5 px-3">
-        <div className="h-full box-border bg-white shadow-lg rounded-lg p-6 grid grid-rows-[auto_1fr]">
+        <div className="h-full box-border bg-white shadow-lg rounded-lg p-6 grid grid-rows-[auto_1fr_auto_1fr]">
           <div className="flex items-center justify-between relative">
             <h2 className="text-3xl font-semibold text-slate-700">
               {t("posts.title")}
@@ -30,7 +33,7 @@ const UserPostOverview: React.FC = () => {
           </div>
           <div className="border-t-2 border-slate-600 w-full h-0 min-h-full max-h-full flex flex-col overflow-y-auto">
             {posts.length === 0 && (
-              <p className="text-slate-500"> {t("posts.no_post")}</p>
+              <p className="text-slate-500"> {t("posts.no_posts")}</p>
             )}
             {posts.map((post, index) => (
               <div
@@ -49,6 +52,38 @@ const UserPostOverview: React.FC = () => {
                 </div>
                 <div>
                   <p className="text-sm text-slate-500">{post.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex items-center justify-between relative mt-4">
+            <h2 className="text-3xl font-semibold text-slate-700">
+              {t("posts.joined")}
+            </h2>
+          </div>
+          <div className="border-t-2 border-slate-600 w-full h-0 min-h-full max-h-full flex flex-col overflow-y-auto">
+            {joinedPosts.length === 0 && (
+              <p className="text-slate-500"> {t("posts.no_posts")}</p>
+            )}
+            {joinedPosts.map((joinedPost, index) => (
+              <div
+                key={joinedPost.id}
+                className={`w-full h-20 ${
+                  index !== 0 ? "border-t-2 border-slate-400" : ""
+                } p-2 cursor-pointer `}
+              >
+                <div className="flex items-center justify-between">
+                  <h3 className="text-lg font-semibold text-slate-700">
+                    {joinedPost.title}
+                  </h3>
+                  <p className="text-sm text-slate-500">
+                    ({joinedPost.peopleJoined}/{joinedPost.peopleNeeded})
+                  </p>
+                </div>
+                <div>
+                  <p className="text-sm text-slate-500">
+                    {joinedPost.description}
+                  </p>
                 </div>
               </div>
             ))}
