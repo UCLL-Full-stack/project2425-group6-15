e81616f71b921cfa1051a6220069d0d8f3e4098e
HannesBosmans <hannes.bosmans@student.ucll.be>
2024-12-16 13:09:34 +0100

    pin int midden

--- front-end/services/postService.ts
+++ front-end/services/postService.ts
@@ -32,7 +32,25 @@ const getAllPosts = async () => {
     });
   };
 
+  const joinPost = async (id: number) => {
+    let token = sessionStorage.getItem("token");
+    if (!token) {
+      throw new Error("Token not found");
+    }
+    return fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/${id}/join`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        "Cache-Control": "no-cache",
+        ...(process.env.NEXT_PUBLIC_API_KEY && { "x-api-key": process.env.NEXT_PUBLIC_API_KEY }),
+      },
+    });
+  };
+
   export default {
     getAllPosts,
     getPostById,
+    joinPost,
   };
